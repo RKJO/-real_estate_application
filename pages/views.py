236a255322e18c7146e3a3ddf8aaeeb3a2d6851c
RKJO <rkjozwiak@gmail.com>
2018-11-20 19:12:45 +0100

    add choices up to search form"

--- pages/views.py
+++ pages/views.py
@@ -1,16 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.choices import price_choices, bedroom_choices, state_choices
 from listings.models import Listing
+from realtors.models import Realter
 
 def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
     
     ctx = {
-        'listings' : listings
+        'listings' : listings,
+        'state_choices' : state_choices,
+        'price_choices' : price_choices,
+        'bedroom_choices' : bedroom_choices,
     }
     
     return render(request, 'pages/index.html', ctx)
 
 
 def about(request):
-    return render(request, 'pages/about.html')
+    # Get all realtors
+
+    realters = Realter.objects.order_by('-hire_date')
+
+    # Get MVP
+    mvp_realtors = Realter.objects.all().filter(is_mvp=True)
+
+    ctx = {
+        'realtors' : realters, 'mvp_realtors' : mvp_realtors
+    }
+
+    return render(request, 'pages/about.html', ctx)
--- realtors/admin.py
+++ realtors/admin.py
@@ -3,9 +3,11 @@ from django.contrib import admin
 from .models import Realter
 
 class RealterAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'email', 'hire_date')
+    list_display = ('id', 'name', 'is_mvp', 'email', 'hire_date')
     list_display_links = ('id', 'name')
     seagitrch_fields = ('name',)
+    list_editable = ('is_mvp',)
+
     list_pre_page = 25
 
 admin.site.register(Realter, RealterAdmin)
